6262722f5c14f4b5d44ffa2260d0947f7ecb53c3
junhyuk <dejavukk1@gmail.com>
2019-12-03 16:01:52 +0900

    Formatter를 활용하여 날짜 표시, 한글로 인코딩.

--- TodayWrite/Controller/MemoListTableViewController.swift
+++ TodayWrite/Controller/MemoListTableViewController.swift
@@ -11,35 +11,37 @@ import UIKit
 class MemoListTableViewController: UITableViewController {
     
     
+    // 클로저: 반드시 따로 스위프트 문법 공부
+    let formatter: DateFormatter = {
+        
+       let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        
+        return f
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-    
-    
+    // MARK: - Table view data source 테이블뷰를 구현하려면 반드시 필수 사용해야 할 메소드.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         // #warning Incomplete implementation, return the number of rows
-        
-        
         return Memo.dummyMemoList.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         // Configure the cell...
         let target = Memo.dummyMemoList[indexPath.row]
         cell.textLabel?.text = target.content
-        cell.detailTextLabel?.text = target.insertDate.description
+        cell.detailTextLabel?.text = formatter.string(from: target.insertDate)
 
         return cell
     }
