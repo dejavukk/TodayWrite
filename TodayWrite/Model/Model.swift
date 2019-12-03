0f3792914fef80034057234e133f87bac9c5e789
junhyuk <dejavukk1@gmail.com>
2019-12-03 14:55:46 +0900

    Navigation, TableView, MVC 패턴으로 변형.

---
+++ TodayWrite/Model/Model.swift
@@ -0,0 +1,29 @@
+//
+//  Model.swift
+//  TodayWrite
+//
+//  Created by JunHyuk on 2019/12/03.
+//  Copyright © 2019 junhyuk. All rights reserved.
+//
+
+import Foundation
+
+
+class Memo {
+    
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        
+        self.content = content
+        insertDate = Date()
+        
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "청소하기"),
+        Memo(content: "잡지 정기구독.")
+        
+    ]
+}
